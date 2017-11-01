d439224674eb1b48785f36e9b75d6339d2adfa66
Ana Claudia <anacnogueira@gmail.com>
2017-11-01 11:11:41 -0200

    Persiste a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+         self.obj = Subscription(
+            name='Ana Claudia Nogueira',
+            cpf='12345678901',
+            email='anacnogueira@gmail.com',
+            phone='12-998269414'
+         )
+         self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an aout created_at attribute."""
+        self.assertIsInstance(self.obj.created_at, datetime)
+
+
+
